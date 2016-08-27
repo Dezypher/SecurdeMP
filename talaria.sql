e5d2a060def3ebea518679af8313c5e1740ca3bf
Dezypher <Solomon@Solomons-MacBook-Pro.local>
2016-08-27 19:05:31 +0800

    Updated SQL Dump File

--- talaria.sql
+++ talaria.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   `acctype` int(11) DEFAULT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE `account` (
   `mname` varchar(45) DEFAULT NULL,
   `lname` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,6 +43,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (1,'leondojima','qweqwe',1,'leondojima@hotmail.com','Leon','K','Dojima'),(2,'leondojimar','qweqwe',2,'leondojimar@hotmail.com','Leon','K','Dojima'),(3,'leondojimard','qweqwe',1,'leondojimard@hotmail.com','Leon','K','Dojima'),(4,'leondojimsfsard','qweqwe',1,'leondojasfimard@hotmail.com','Leon','K','Dojima'),(5,'admindull','qweqwe',3,'mahmen@gmail.com','Admin','L','Dull'),(7,'prodman','qweqwe',2,'prodman@proman.org','Produck','G','Manijeer'),(8,'johndoe','qweqwe',1,'johndoedoe@doe.tho','John','J.','Doe'),(9,'brobro','qweqwe',1,'brobro@bro.bro','Jimothy','L.','Bro'),(10,'jimm02','qweqwe',1,'jimmy@yahoo.com','Jim','M.','Mim'),(11,'accman','qweqwe',3,'accman@gmail.com','Accounting','M.','Manager'),(12,'produman','qweqwe',2,'produman@gmail.com','Produ','M','Man');
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,14 +55,13 @@ DROP TABLE IF EXISTS `billing_addr`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `billing_addr` (
-  `accountID` int(11) NOT NULL,
+  `accountID` int(11) DEFAULT NULL,
   `houseNo` varchar(45) DEFAULT NULL,
   `street` varchar(45) DEFAULT NULL,
   `subdivision` varchar(45) DEFAULT NULL,
   `city` varchar(45) DEFAULT NULL,
   `postalCode` varchar(45) DEFAULT NULL,
-  `country` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`accountID`)
+  `country` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -71,6 +71,7 @@ CREATE TABLE `billing_addr` (
 
 LOCK TABLES `billing_addr` WRITE;
 /*!40000 ALTER TABLE `billing_addr` DISABLE KEYS */;
+INSERT INTO `billing_addr` VALUES (9,'12','Bro','Brodivision','Brocity',NULL,'Brountry'),(10,'1','Taft','Taft','Manila',NULL,'Philippines');
 /*!40000 ALTER TABLE `billing_addr` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,6 +97,27 @@ CREATE TABLE `cart` (
 LOCK TABLES `cart` WRITE;
 /*!40000 ALTER TABLE `cart` DISABLE KEYS */;
 /*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fpproducts`
+--
+
+DROP TABLE IF EXISTS `fpproducts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fpproducts` (
+  `productID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fpproducts`
+--
+
+LOCK TABLES `fpproducts` WRITE;
+/*!40000 ALTER TABLE `fpproducts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fpproducts` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -130,14 +152,14 @@ DROP TABLE IF EXISTS `products`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `type` int(11) DEFAULT NULL,
-  `description` varchar(45) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
   `price` float DEFAULT NULL,
-  `imagePath` varchar(45) DEFAULT NULL,
+  `imagePath` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -146,6 +168,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Killer Crocs',4,'If looks can kill, so can these! Made from 100% pure crocodile leather. Made in Gotham.',1299.99,'http://vignette3.wikia.nocookie.net/arkhamcity/images/0/04/Killer_Croc_(Arkham_Origins).jpg/revision/latest?cb=20160229225831'),(2,'Not Cool Shoes',2,'Not too cool for you :P',1337,'http://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2015/12/17/103250871-eminemXcarhartt_airjordan4.600x400.jpg?v=1450371134'),(3,'Save the Whales',4,'Dolphin lives matter. Save the narwhals.',0.99,'http://www.thinkgeek.com/images/products/zoom/1cf9_shark_plush_slippers.jpg'),(4,'Old School',3,'Damn Daniel. Back at it again. With them sandals.',19.8,'http://macys-o.scene7.com/is/image/MCY/products/5/optimized/3335955_fpx.tif?op_sharpen=1&wid=400&hei=489&fit=fit,1&$filterlrg$'),(5,'Cowboy Boots',1,'May or may not come with snakes in the boots.',849.99,'http://img15.deviantart.net/84fa/i/2012/363/3/6/boots_with_spurs_by_animevegas-d5pk66x.jpg');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -157,13 +180,13 @@ DROP TABLE IF EXISTS `review`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `review` (
-  `id` int(11) NOT NULL,
-  `authorID` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(100) DEFAULT NULL,
   `productID` int(11) DEFAULT NULL,
   `review` varchar(200) DEFAULT NULL,
   `rating` float DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -172,6 +195,7 @@ CREATE TABLE `review` (
 
 LOCK TABLES `review` WRITE;
 /*!40000 ALTER TABLE `review` DISABLE KEYS */;
+INSERT INTO `review` VALUES (1,'leondojima',1,'Best Crocs Ever',5),(2,'brobro',8,'Quack :(',1),(3,'jimm02',1,'I like this not really :)',4);
 /*!40000 ALTER TABLE `review` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -194,6 +218,7 @@ CREATE TABLE `sales` (
 
 LOCK TABLES `sales` WRITE;
 /*!40000 ALTER TABLE `sales` DISABLE KEYS */;
+INSERT INTO `sales` VALUES (1,2),(2,1),(3,1),(4,4),(5,1),(6,1),(7,1),(8,1),(9,0);
 /*!40000 ALTER TABLE `sales` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -205,14 +230,13 @@ DROP TABLE IF EXISTS `shipping_addr`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `shipping_addr` (
-  `accountID` int(11) NOT NULL,
+  `accountID` int(11) DEFAULT NULL,
   `houseNo` varchar(45) DEFAULT NULL,
   `street` varchar(45) DEFAULT NULL,
   `subdivision` varchar(45) DEFAULT NULL,
   `city` varchar(45) DEFAULT NULL,
   `postalCode` varchar(45) DEFAULT NULL,
-  `country` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`accountID`)
+  `country` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -222,6 +246,7 @@ CREATE TABLE `shipping_addr` (
 
 LOCK TABLES `shipping_addr` WRITE;
 /*!40000 ALTER TABLE `shipping_addr` DISABLE KEYS */;
+INSERT INTO `shipping_addr` VALUES (8,'24','Avocado St','Brink','Las Pineapples',NULL,'Chile'),(8,'24','Avocado St','Brink','Las Pineapples',NULL,NULL),(9,'12','Bro','Brodivision','Brocity',NULL,'Brountry'),(10,'1','Taft','Taft','Manila',NULL,'Philippines');
 /*!40000 ALTER TABLE `shipping_addr` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -244,6 +269,7 @@ CREATE TABLE `stock` (
 
 LOCK TABLES `stock` WRITE;
 /*!40000 ALTER TABLE `stock` DISABLE KEYS */;
+INSERT INTO `stock` VALUES (1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0);
 /*!40000 ALTER TABLE `stock` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -256,4 +282,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-08-11 12:04:38
+-- Dump completed on 2016-08-27 19:04:40
